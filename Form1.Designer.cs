909f4cbd86f019feba365e46f10db415218ab371
satishchandra.d <satishchandra.d@celestialsys.com>
2024-01-16 08:19:34 +0530

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace TerraformLauncherApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRunTerraform = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnRunTerraform
+            // 
+            this.btnRunTerraform.Location = new System.Drawing.Point(231, 138);
+            this.btnRunTerraform.Name = "btnRunTerraform";
+            this.btnRunTerraform.Size = new System.Drawing.Size(258, 74);
+            this.btnRunTerraform.TabIndex = 0;
+            this.btnRunTerraform.Text = "Run Terrraform";
+            this.btnRunTerraform.UseVisualStyleBackColor = true;
+            this.btnRunTerraform.Click += new System.EventHandler(this.btnRunTerraform_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnRunTerraform);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRunTerraform;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TerraformLauncherApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnRunTerraform_Click(object sender, EventArgs e)
+        {
+            // Disable the button while Terraform commands are running
+            btnRunTerraform.Enabled = false;
+
+            // Run Terraform init and apply asynchronously
+            await RunTerraformAsync();
+
+            // Enable the button after Terraform commands are completed
+            btnRunTerraform.Enabled = true;
+        }
+
+        private async Task RunTerraformAsync()
+        {
+            // Assuming the Terraform script is in C:\Temp folder
+            string terraformScriptDirectory = @"C:\Temp\Terraform\Script";
+
+            // Run 'terraform init' command
+            await RunCommandAsync("terraform", "init", terraformScriptDirectory);
+
+            // Run 'terraform apply' command
+            await RunCommandAsync("terraform", "apply", terraformScriptDirectory);
+        }
+
+        private async Task RunCommandAsync(string command, string arguments, string workingDirectory)
+        {
+            using (Process process = new Process())
+            {
+                process.StartInfo.FileName = command;
+                process.StartInfo.Arguments = arguments;
+                process.StartInfo.WorkingDirectory = workingDirectory;
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.RedirectStandardOutput = true;
+
+                // Handle output data
+                process.OutputDataReceived += (sender, e) =>
+                {
+                    if (!string.IsNullOrEmpty(e.Data))
+                    {
+                        // You can handle the output data as needed (e.g., log it, display it, etc.)
+                        Console.WriteLine(e.Data);
+                    }
+                };
+
+                // Start the process
+                process.Start();
+
+                // Begin asynchronous read of the output stream
+                process.BeginOutputReadLine();
+
+                // Wait for the process to exit
+                await process.WaitForExitAsync();
+            }
+        }
+    }
+    public static class ProcessExtensions
+    {
+        public static Task<int> WaitForExitAsync(this Process process)
+        {
+            var tcs = new TaskCompletionSource<int>();
+            process.EnableRaisingEvents = true;
+            process.Exited += (sender, args) => tcs.TrySetResult(process.ExitCode);
+            return tcs.Task;
+        }
+    }
+}
